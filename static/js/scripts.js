aac47e8f343b0050e062a2a48efb74f8f613188c
asymmetric <asymmetric@autistici.org>
2011-05-12 01:27:54 +0200

    handle index edge case

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,16 @@
-//domReady(function() {
-//  var current_page = document.URL.split('/').pop();
-//  var page;
-//
-//  for (page in $('#sidebar a')) {
-//    if (page.getAttribute('href') == current_page) {
-//      page.className = 'current';
-//    }
-//  }
-//});
-
 head.ready(function() {
   var current_page = document.URL.split('/').pop();
-  var page;
-  var n;
 
-  snack.wrap('#sidebar a').each(function(el, i) {
-    if (el.getAttribute('href') == current_page) {
-      snack.wrap(el).addClass('current');
-    }
-  });
+  if (current_page === "") {
+    var index = snack.wrap('#sidebar a:first-child')[0];
+    snack.wrap(index).addClass('current');
+  }
+  else {
+    snack.wrap('#sidebar a').each(function(el, i) {
+      var attr = el.getAttribute('href');
+      if (attr == current_page) {
+        snack.wrap(el).addClass('current');
+      }
+    });
+  }
 });
